e691b66c8ab1c7aa64320430bc12b4b14f704e0b
Perry Alexander <palexand@ku.edu>
2025-07-08 16:39:33 -0500

    fixed tail recursive triangle & added loop triangle

--- src/triangle.rs
+++ src/triangle.rs
@@ -78,6 +78,50 @@ fn mut_triangle(n: u32, z: &mut u32)
                 *z = *z + n }
     }
 
+fn tail_triangle(n: u32, idx: u32, sum: &mut u32)
+        requires
+            idx <= n,
+            *old(sum) == triangle(idx as nat),
+            triangle(n as nat) < 0x1_0000_0000,
+        ensures
+            *sum == triangle(n as nat),
+        decreases n - idx,
+    {
+        if idx < n {
+            let idx = idx + 1;
+            assert(*sum + idx < 0x1_0000_0000) by {
+                triangle_is_monotonic(idx as nat, n as nat);
+            }
+            // assert(*sum + idx < 0x1_0000_0000)
+            *sum = *sum + idx;
+            tail_triangle(n,idx,sum);
+        }
+    }
+
+fn loop_triangle(n:u32) -> (sum:u32)
+    requires
+        triangle(n as nat) < 0x1_0000_0000,
+    ensures
+        sum == triangle(n as nat),
+{
+    let mut sum: u32 = 0;
+    let mut idx: u32 = 0;
+    while idx < n
+        invariant
+            idx <= n,
+            sum == triangle(idx as nat),
+            triangle(n as nat) < 0x1_0000_0000,
+        decreases n - idx,
+    {
+        idx = idx + 1;
+        assert(sum + idx < 0x1_0000_0000) by {
+            triangle_is_monotonic(idx as nat, n as nat);
+        }
+        sum = sum + idx;
+    }
+    return sum
+}
+
     fn main(){
 //        assert(triangle(3) < 0x1_0000_0000) by
 //            {reveal_with_fuel(triangle,20)};
